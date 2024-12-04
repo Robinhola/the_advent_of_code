0d99f15089c0558c589bc4292f0896186b982ea6
Robin Elrharbi-Fleury <robin.elrharbifleury@gmail.com>
2024-12-04 16:55:46 +0530

    2024day04 simpler

--- 2024_04_ocaml/advent/lib/solve.ml
+++ 2024_04_ocaml/advent/lib/solve.ml
@@ -27,43 +27,54 @@ module Coord = struct
   include T
   module Map = Map.Make (T)
   module Hash_set = Hash_set.Make (T)
+
+  let to_tuple t = t.x, t.y
 end
-
-type coord = Coord.t [@@deriving sexp, compare, equal, hash]
-
-let to_tuple (coord : Coord.t) = coord.x, coord.y
 
 type matrix =
   { words : char array array
-  ; dims : coord
+  ; dims : Coord.t
   }
 [@@deriving sexp]
 
-let parse l = List.map l ~f:String.to_array |> List.to_array
-
 let parse' l =
-  let words = parse l in
+  let words = List.map l ~f:String.to_array |> List.to_array in
   let dims = { Coord.x = Array.length words.(0); y = Array.length words } in
   { words; dims }
 ;;
 
-type dir =
-  | Up
-  | Down
-  | Left
-  | Right
-  | Up_right
-  | Up_left
-  | Down_right
-  | Down_left
-[@@deriving sexp, enumerate]
+module Dir = struct
+  type t =
+    | Up
+    | Down
+    | Left
+    | Right
+    | Up_right
+    | Up_left
+    | Down_right
+    | Down_left
+  [@@deriving sexp, enumerate]
+
+  let for_all ~f = List.map ~f all
+
+  let other_way = function
+    | Up -> Down
+    | Down -> Up
+    | Left -> Right
+    | Right -> Left
+    | Up_right -> Down_left
+    | Up_left -> Down_right
+    | Down_right -> Up_left
+    | Down_left -> Up_right
+  ;;
+end
 
 let next max_coord coord direction =
-  let mx, my = to_tuple max_coord in
-  let x, y = to_tuple coord in
+  let mx, my = Coord.to_tuple max_coord in
+  let x, y = Coord.to_tuple coord in
   let ox, oy =
     match direction with
-    | Up -> 0, -1
+    | Dir.Up -> 0, -1
     | Down -> 0, 1
     | Left -> -1, 0
     | Right -> 1, 0
@@ -79,7 +90,7 @@ let next max_coord coord direction =
   | nx, ny -> Some { Coord.x = nx; y = ny }
 ;;
 
-let rec look ~matrix (coord : coord) direction chars =
+let rec look ~matrix (coord : Coord.t) direction chars =
   let value = matrix.words.(coord.y).(coord.x) in
   let is_equal = Char.equal value in
   match chars with
@@ -92,75 +103,63 @@ let rec look ~matrix (coord : coord) direction chars =
   | _ -> 0
 ;;
 
-let for_all_dir f = List.map ~f all_of_dir
+let all_indices matrix =
+  List.cartesian_product (List.range 0 matrix.dims.x) (List.range 0 matrix.dims.y)
+;;
+
+let sum (l : 'a list) = List.sum (module Int) l
 
 let part1 (lines : string list) =
   let word = String.to_list "XMAS" in
   let matrix = parse' lines in
-  let coords =
-    List.cartesian_product (List.range 0 matrix.dims.x) (List.range 0 matrix.dims.y)
-  in
-  List.sum
-    (module Int)
-    coords
-    ~f:(fun (x, y) ->
-      for_all_dir (fun dir -> look ~matrix { Coord.x; y } dir word)
-      |> List.sum (module Int) ~f:Fn.id)
-;;
-
-let other_way = function
-  | Up -> Down
-  | Down -> Up
-  | Left -> Right
-  | Right -> Left
-  | Up_right -> Down_left
-  | Up_left -> Down_right
-  | Down_right -> Up_left
-  | Down_left -> Up_right
+  let coords = all_indices matrix in
+  sum coords ~f:(fun (x, y) ->
+    Dir.for_all ~f:(fun dir -> look ~matrix { x; y } dir word)
+    |> List.sum (module Int) ~f:Fn.id)
 ;;
 
 let start_of max_coord coord dir =
-  let dir = other_way dir in
+  (* this works because we only go one in the opposite direction to find the start *)
+  let dir = Dir.other_way dir in
   next max_coord coord dir
 ;;
 
-let xlook ~matrix (center : coord) =
+let xlook ~matrix (center : Coord.t) =
   let chars = "MAS" |> String.to_list in
   let is_valid direction =
     let coord = start_of matrix.dims center direction in
     Option.map coord ~f:(fun coord -> look ~matrix coord direction chars |> Int.equal 1)
     |> Option.value ~default:false
   in
-  [ Down_right, Up_right; Up_right, Up_left; Up_left, Down_left; Down_left, Down_right ]
-  |> List.filter_map ~f:(fun (a, b) ->
-    if is_valid a && is_valid b
-    then (
-      let result = center, a, b in
-      Some result)
-    else None)
+  List.filter_map
+    Dir.
+      [ Down_right, Up_right
+      ; Up_right, Up_left
+      ; Up_left, Down_left
+      ; Down_left, Down_right
+      ]
+    ~f:(fun (a, b) -> if is_valid a && is_valid b then Some (center, a, b) else None)
 ;;
 
 let part2' (lines : string list) =
   let matrix = parse' lines in
-  let coords =
-    List.cartesian_product (List.range 0 matrix.dims.x) (List.range 0 matrix.dims.y)
-  in
-  List.map coords ~f:(fun (x, y) ->
-    let coord = { Coord.x; y } in
-    xlook ~matrix coord)
+  let coords = all_indices matrix in
+  List.map coords ~f:(fun (x, y) -> xlook ~matrix { x; y })
 ;;
 
-let part2 (lines : string list) = List.sum (module Int) (part2' lines) ~f:List.length
+let part2 (lines : string list) = sum ~f:List.length (part2' lines)
 
 let%expect_test _ =
   print_s [%message (part1 sample_1 : int)];
+  print_s [%message (part2 sample_1 : int)];
   part2' sample_1
   |> List.filter ~f:(Fn.compose not List.is_empty)
   |> List.concat
-  |> List.iter ~f:(fun xmas -> print_s [%message (xmas : coord * dir * dir)]);
+  |> List.iter ~f:(fun xmas -> print_s [%message (xmas : Coord.t * Dir.t * Dir.t)]);
   [%expect
     {|
     ("part1 sample_1" 18)
+    ("part2 sample_1" 9)
     (xmas (((x 1) (y 7)) Up_right Up_left))
     (xmas (((x 2) (y 1)) Down_right Up_right))
     (xmas (((x 2) (y 3)) Down_right Up_right))
