5d7d66623af556d8ec293008d38eb5ffe8f96d17
Robin Elrharbi-Fleury <robin.elrharbifleury@gmail.com>
2024-12-07 11:16:14 +0530

    Changes to default project

--- ocaml_default_project/advent/lib/solve.ml
+++ ocaml_default_project/advent/lib/solve.ml
@@ -1,17 +1,19 @@
 open! Base
 open! Core
 
-let sample_1 = ""
+let sample_1 = {||} |> String.split_lines
 
 let part1 (lines : string list) =
   List.iteri lines ~f:(fun i line -> print_s [%message (i : int) (line : string)]);
   0
 ;;
 
-let part2 (_lines : string list) = 0
+let part2 (lines : string list) =
+  let _ = lines in
+  0
+;;
 
 let%expect_test _ =
-  let result = String.split_lines sample_1 |> part1 in
-  print_s [%message (result : int)];
-  [%expect {| (result 0) |}]
+  print_s [%message (part1 sample_1 : int)];
+  print_s [%message (part2 sample_1 : int)]
 ;;
