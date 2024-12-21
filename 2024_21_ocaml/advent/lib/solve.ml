150921a186c6feaa848762e0ca978785425f98b4
Robin Elrharbi-Fleury <robin.elrharbifleury@gmail.com>
2024-12-21 19:57:52 +0000

    Part2-not working

--- 2024_21_ocaml/advent/lib/solve.ml
+++ 2024_21_ocaml/advent/lib/solve.ml
@@ -22,6 +22,10 @@ module Cache = Hashtbl.Make (struct
     type t = char * char [@@deriving sexp, compare, hash]
   end)
 
+module Cache' = Hashtbl.Make (struct
+    type t = char list * int [@@deriving sexp, compare, hash]
+  end)
+
 let sample_1 =
   {|029A
 980A
@@ -64,11 +68,6 @@ let rec find ~m ~seen ~bests to_ queue =
         |> Option.map ~f:(fun c -> c, i + 1, from_dir_to_dir dir :: path))
       |> Queue.enqueue_all queue;
       find ~m ~seen ~bests to_ queue))
-;;
-
-let part2 (lines : string list) =
-  let _ = lines in
-  0
 ;;
 
 let all_shortest_paths m from to_ =
@@ -159,15 +158,19 @@ let%expect_test _ =
 ;;
 
 let combine chemin =
-  let rec combine current = function
-    | _e :: [] -> current
-    | [] -> assert false
+  let rec combine = function
+    | _ :: [] | [] -> assert false
+    | [ s; e ] ->
+      let key = s, e in
+      let best = Hashtbl.find_exn best_directional_paths key in
+      List.concat [ best; [ 'A' ] ]
     | s :: e :: rest ->
       let key = s, e in
       let best = Hashtbl.find_exn best_directional_paths key in
-      combine (List.concat [ current; best; [ 'A' ] ]) (e :: rest)
+      let value = combine (e :: rest) in
+      List.concat [ best; [ 'A' ]; value ]
   in
-  combine [] ('A' :: chemin)
+  combine ('A' :: chemin)
 ;;
 
 let pairs s =
@@ -179,19 +182,15 @@ let pairs s =
   pairs [] (String.to_list s |> List.map ~f:Char.to_string)
 ;;
 
-let algo code =
+let algo n code =
   let pairs = pairs code in
   let possibles = List.map pairs ~f:(fun p -> p, Hashtbl.find_exn all_numeric_paths p) in
   let bests =
     List.map possibles ~f:(fun (_p, options) ->
       List.min_elt options ~compare:(fun a b ->
-        let p1a = a in
-        let p2a = combine p1a in
-        let p3a = combine p2a in
-        let p1b = b in
-        let p2b = combine p1b in
-        let p3b = combine p2b in
-        Int.compare (List.length p3a) (List.length p3b))
+        let pna = (Fn.apply_n_times ~n combine) a in
+        let pnb = (Fn.apply_n_times ~n combine) b in
+        Int.compare (List.length pna) (List.length pnb))
       |> Option.value_exn)
     |> List.map ~f:String.of_list
     |> String.concat ~sep:"A"
@@ -206,20 +205,104 @@ let num_part s =
   | _ -> assert false
 ;;
 
-let part1 (lines : string list) =
+let part1' (lines : string list) =
   List.sum
     (module Int)
     lines
     ~f:(fun code' ->
       let code = "A" ^ code' in
       let n = num_part code in
-      let p = algo code in
+      let p = algo 2 code in
       print_endline (code' ^ ": " ^ p);
       let l = String.length p in
       l * n)
 ;;
 
+let partx ~cache ~total_iterations ~batch_size (word : char list) =
+  (*let () =*)
+  (*  match List.hd_exn word |> Char.equal 'A', List.last_exn word |> Char.equal 'A' with*)
+  (*  | true, true -> ()*)
+  (*  | false, _ | _, false -> raise_s [%message (word : char list)]*)
+  (*in*)
+  (*print_s [%message (word : char list)];*)
+  assert (total_iterations % batch_size = 0);
+  let goal = total_iterations / batch_size in
+  let rec partx' ~goal i x =
+    let iterations_so_far = i * batch_size in
+    let key = x, iterations_so_far in
+    match Hashtbl.find cache key with
+    | Some value -> value
+    | None ->
+      let value =
+        match i = goal with
+        | true -> Fn.apply_n_times ~n:batch_size combine x |> List.length
+        | false ->
+          let next_batch =
+            Fn.apply_n_times ~n:batch_size combine x
+            |> String.of_list
+            |> fun s ->
+            String.prefix s (String.length s - 1)
+            |> String.split ~on:'A'
+            |> List.map ~f:(fun s -> s ^ "A")
+            |> List.map ~f:String.to_list
+          in
+          List.sum (module Int) ~f:(partx' ~goal (i + 1)) next_batch
+      in
+      Hashtbl.add_exn cache ~key ~data:value;
+      value
+  in
+  List.sum (module Int) ~f:(partx' ~goal 1) [ word ]
+;;
+
+let partx' ~cache ~total_iterations ~batch_size code =
+  let bests =
+    pairs code
+    |> List.map ~f:(fun p -> p, Hashtbl.find_exn all_numeric_paths p)
+    |> List.map ~f:(fun (_p, options) ->
+      List.min_elt options ~compare:(fun a b ->
+        let a = List.concat [ a; [ 'A' ] ] in
+        let b = List.concat [ b; [ 'A' ] ] in
+        let pna = partx ~cache ~total_iterations ~batch_size a in
+        let pnb = partx ~cache ~total_iterations ~batch_size b in
+        Int.compare pna pnb)
+      |> Option.value_exn)
+    |> List.map ~f:String.of_list
+    |> String.concat ~sep:"A"
+  in
+  let bests = bests ^ "A" in
+  partx ~cache ~total_iterations ~batch_size (String.to_list bests)
+;;
+
+let partx'' lines cache ~batch_size total =
+  let value =
+    List.sum
+      (module Int)
+      lines
+      ~f:(fun code' ->
+        let code = "A" ^ code' in
+        let n = num_part code in
+        let l = partx' ~cache ~total_iterations:total ~batch_size code in
+        l * n)
+  in
+  (*print_s [%message (cache : int Cache'.t)];*)
+  value
+;;
+
+let cache = Cache'.create ()
+let part1 (lines : string list) = partx'' lines cache ~batch_size:1 2
+let cache = Cache'.create ()
+let part2 (lines : string list) = partx'' lines cache ~batch_size:1 25
+(*let part2 (_lines : string list) = 0*)
+(* 184716
+
+high 267682029656984
+
+     260675562033542
+
+low  106936356357428 *)
+
 let%expect_test _ =
+  print_s [%message (part1' sample_1 : int)];
   print_s [%message (part1 sample_1 : int)];
   print_s [%message (part2 sample_1 : int)];
   [%expect
@@ -229,7 +312,8 @@ let%expect_test _ =
     179A: v<<A>>^A<vA<A>>^AAvAA<^A>Av<<A>>^AAvA^A<vA>^AA<A>Av<<A>A>^AAAvA<^A>A
     456A: v<<A>>^AA<vA<A>>^AAvAA<^A>A<vA>^A<A>A<vA>^A<A>Av<<A>A>^AAvA<^A>A
     379A: v<<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>Av<<A>A>^AAAvA<^A>A
+    ("part1' sample_1" 126384)
     ("part1 sample_1" 126384)
-    ("part2 sample_1" 0)
+    ("part2 sample_1" 175343041201758)
     |}]
 ;;
