e7da99bab7dc0672aa16769f88617bd7407b2a60
Robin Elrharbi-Fleury <robin.elrharbifleury@gmail.com>
2025-12-06 12:36:52 +0000

    with transpose

--- 2025_06_ocaml/advent/lib/solve.ml
+++ 2025_06_ocaml/advent/lib/solve.ml
@@ -48,53 +48,39 @@ let part1 (lines : string list) =
   sum vals
 ;;
 
-let rec add_column t coord vals =
-  let x = Matrix.get t coord in
-  let vals = x :: vals in
-  match Matrix.next t coord Dir.Down with
-  | Some c ->
-    let x = Matrix.get t c in
-    add_column t c vals
-  | None -> vals
+let rec split_blocks blocks current results =
+  match blocks with
+  | [] -> current :: results
+  | "" :: rest -> split_blocks rest [] (current :: results)
+  | line :: rest -> split_blocks rest (line :: current) results
 ;;
 
-let col_to_num col =
-  let s =
-    List.drop col 1
-    |> List.rev
-    |> String.of_list
-    |> String.substr_replace_all ~pattern:" " ~with_:""
-  in
-  if String.is_empty s then 0 else Int.of_string s
-;;
-
-let rec split_on_zero nums cur lists =
-  match nums with
-  | [] -> lists @ [ cur ]
-  | 0 :: rest -> split_on_zero rest [] (lists @ [ cur ])
-  | n :: rest -> split_on_zero rest (n :: cur) lists
+let rec process_block vals block =
+  match block with
+  | [ last_value ] ->
+    let value = String.drop_suffix last_value 1 |> Int.of_string in
+    let sign = String.drop_prefix last_value (String.length last_value - 1) in
+    let vals = value :: vals in
+    (match sign with
+     | "+" -> sum vals
+     | "*" -> mul vals
+     | _ -> raise_s [%message "impossible"])
+  | value :: rest ->
+    let vals = Int.of_string value :: vals in
+    process_block vals rest
+  | _ -> raise_s [%message "impossible"]
 ;;
 
 let part2 (lines : string list) =
   let t = Matrix.parse lines in
-  let num_of_cols = List.hd_exn lines |> String.length in
-  let cols = Array.init num_of_cols ~f:(fun _ -> []) in
-  Array.iteri cols ~f:(fun i _ ->
-    let col = add_column t (Coord.of_tuple (i, 0)) [] in
-    Array.set cols i col);
-  debug [%message (cols : char list array)];
-  let nums = Array.map cols ~f:col_to_num |> Array.to_list in
-  debug [%message (nums : int list)];
-  let signs =
-    List.map lines ~f:(fun s ->
-      String.split s ~on:' ' |> List.filter ~f:(fun s -> not (String.is_empty s)))
-    |> List.rev
-    |> List.hd_exn
+  let t = Matrix.transpose t in
+  let sanitized =
+    Matrix.to_list t
+    |> List.map ~f:String.of_list
+    |> List.map ~f:(String.substr_replace_all ~pattern:" " ~with_:"")
   in
-  let cols = split_on_zero nums [] [] |> Array.of_list in
-  debug [%message (cols : int list array) (signs : string list)];
-  let vals = map_signs_to_cols signs cols in
-  sum vals
+  let blocks = split_blocks sanitized [] [] in
+  sum (List.map blocks ~f:(process_block []))
 ;;
 
 let%expect_test _ =
@@ -105,14 +91,9 @@ let%expect_test _ =
     {|
     (l ((123 328 51 64) (45 64 387 23) (6 98 215 314) (* + * +)))
     ((cols ((123) (328) (51) (64))) (signs (* + * +)))
-    ((cols ((123 45 6) (328 64 98) (51 387 215) (64 23 314))) (signs (* + * +)))
+    ((cols ((6 45 123) (98 64 328) (215 387 51) (314 23 64))) (signs (* + * +)))
     ("part1 sample_1" 4277556)
-    (cols
-     ((* " " " " 1) (" " " " 4 2) (" " 6 5 3) (" " " " " " " ") (+ 9 6 3)
-      (" " 8 4 2) (" " " " " " 8) (" " " " " " " ") (* 2 3 " ") (" " 1 8 5)
-      (" " 5 7 1) (" " " " " " " ") (+ 3 2 6) (" " 1 3 4) (" " 4 " " " ")))
-    (nums (1 24 356 0 369 248 8 0 32 581 175 0 623 431 4))
-    ((cols ((356 24 1) (8 248 369) (175 581 32) (4 431 623))) (signs (* + * +)))
+    (blocks ((4 431 623+) (175 581 32*) (8 248 369+) (356 24 1*)))
     ("part2 sample_1" 3263827)
     |}]
 ;;
